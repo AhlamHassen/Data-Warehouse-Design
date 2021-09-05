0fe18efc442612b0edb1a1b734150f7f4f0f3488
AhlamHassen <ahlamsaleh199700@gmail.com>
2021-09-05 20:02:53 +1000

    Successfully created Patient table

--- Data Warehouse DDL.sql
+++ Data Warehouse DDL.sql
@@ -12,3 +12,24 @@ CREATE TABLE Staff(
     StaffType        NVARCHAR(50) NOT NULL,
     CONSTRAINT PK_DWStaffID PRIMARY KEY (DWStaffID)
 );
+
+
+-- Each entry in this table represents a patient’s details.
+CREATE TABLE Patient(
+    DWPatientID          INT IDENTITY(1,1) NOT NULL,
+    DWSourceDB           NVARCHAR(50) NOT NULL,
+    URNumber             NVARCHAR(50) NOT NULL,
+    Gender               NVARCHAR(50) NOT NULL,
+    YearOB               DATETIME NOT NULL,
+    Suburb               NVARCHAR(50) NOT NULL,
+    PostCode             NVARCHAR(4) NOT NULL,
+    CountryOfBirth       NVARCHAR(50) NOT NULL,
+    PreferedLanguage     NVARCHAR(50) NOT NULL,
+    LivesAlone           BIT NOT NULL,
+    Active               BIT NOT NULL,
+    CategoryName         NVARCHAR(50) NOT NULL,
+    Diagnosis            NVARCHAR(500) NOT NULL,
+    ProcedureDate        DATETIME NULL,
+    CONSTRAINT PK_DWPatientID PRIMARY KEY (DWPatientID)
+);
+
